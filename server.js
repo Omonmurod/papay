d043f3dfd28f6a0a88355267e421d14e3676c50f
Omonmurod <omonmurod@gmail.com>
2023-05-27 14:31:18 +0900

    feat: the form of project's express part

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString = 
+  "mongodb+srv://Jacob:Beshkent19920711@cluster0.wsdr7pe.mongodb.net/Papay";
+
+mongodb.connect(
+  connectionString, 
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }, 
+  (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed!");
+      module.exports = client;  //????
+      const app = require("./app");
+      const server = http.createServer(app);  //barcha zarposlar shu yerga keladi(app bu yerda nodejs ning single thredi) app ExpressJS ni http protokolini serverda yaratib olish
+      let PORT = 3013;
+      //3000 portdan kelayotgan serverni listen qil listen bu method
+      server.listen(PORT, function() {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
+
